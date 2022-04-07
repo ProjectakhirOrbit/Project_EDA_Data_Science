ad3dfc931a4a6360b126c32e3ae4e46d3a3fd4ca
ray-code2 <raymondtjahyadi00@gmail.com>
2022-04-07 19:52:17 +0700

    Menambahkan Scatter Plot dan Bar Plot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 #import Libraries yang diperlukan
+from matplotlib.pyplot import text
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -26,41 +26,44 @@ df = load_data()
 state_select = st.sidebar.selectbox('Select a Country', df['Entity'].unique())
 selected_state = df[df['Entity'] == state_select]
 
+
+column_names = ["Entity","Code","Year","Causes name","Causes Full Description","Death Numbers"]
+
+selected_state = selected_state.reindex(columns=column_names)
+
+st.title('analisis Penyebab Kematian di dunia')
+st.write(selected_state)
+col1, col2 , col3 = st.columns(3)
+
+with col1:
+    st.write(selected_state['Entity'])
+
+with col2:
+    st.write(selected_state['Causes Full Description'])
+with col3:
+    st.write(selected_state['Year'])
+
+bar_graph_Cause = px.bar(selected_state, x='Causes name', y='Death Numbers'.format(int),color='Causes name', width=1000, height=600)
+st.plotly_chart(bar_graph_Cause)
+
 def test_asc():
     visualization1 = df['Year'].sort_values(ascending=True)
     visualization2 = list(set(visualization1))
-    visualization3 = st.sidebar.selectbox('Select Year ' , visualization2)
+    visualization3 = st.selectbox('Select Year ' , visualization2)
     return visualization3
 
 visualization3 = test_asc()
 selected_year = df[df['Year'] == visualization3]
+st.markdown('Scatter plot Jumlah Kematian yang disebabkan oleh penyakit berdasarkan tahun ke tahun')
+st.markdown(visualization3)
+scatter_graph_year = px.scatter(selected_year,x='Causes name', y='Death Numbers' , color='Causes name', width=1000, height=600)
+
+scatter_graph_year.update_traces(marker=dict(size=10,
+                              line=dict(width=2,
+                                        color='DarkSlateGrey')),
+                  selector=dict(mode='markers'))
+st.plotly_chart(scatter_graph_year)
 
 
-st.markdown("select by Year")
-st.write(selected_year)
 
 
-st.markdown("select by State")
-st.write(selected_state)
-
-
-#bar_chart = px.bar(df , x='Death Numbers', y= selected_state)
-#st.plotly_chart(bar_chart)
-
-# st.title('Region Medical Cost Level analysis')
-
-# def get_total_dataframe(df):
-#    total_dataframe = pd.DataFrame({
-  #      'Status':['age','sex','bmi','children','smoker','region','charges'],
- #       'Number of cases': (df.iloc[0]['age'],
-    #                        df.iloc[0]['sex'],
-     #                       df.iloc[0]['bmi'],
-     #                       df.iloc[0]['children'],
-      #                      df.iloc[0]['smoker'],
-      #                      df.iloc[0]['region'],
-       #                     df.iloc[0]['charges'],
-       #                     )
-  #  })
-   # return total_dataframe
-
-#state_total = get_total_dataframe(selected_state)
